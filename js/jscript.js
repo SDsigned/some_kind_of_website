a6192c06bb3c41f0eb3bed0c13d66acde833630b
SDsigned <sd.signed@gmail.com>
2018-05-04 08:25:34 +0200

    Initial Commit

---
+++ js/jscript.js
@@ -0,0 +1,70 @@
+function Light()
+{   
+document.body.style.background= "url('img/light_bg.jpg') center no-repeat";
+document.body.style.backgroundSize= "cover";
+        var elements = document.getElementsByClassName("sidebar")
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.background="url('img/20p_white_bg.png')";
+        }
+
+        var elements = document.getElementsByClassName("sidebar_menu, some_tekst")
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.color="#2f2f2f";
+            elements[i].style.textShadow="none";
+        }
+
+}
+
+function Dark()
+{   
+document.body.style.background= "url('img/dark_bg.jpg') center no-repeat";
+document.body.style.backgroundSize= "cover";
+var elements = document.getElementsByClassName("sidebar")
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.background="url('img/20p_bg.png')";   
+        }
+
+        var elements = document.getElementsByClassName("some_tekst")
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.color="whitesmoke";
+            elements[i].style.textShadow="1px 1px 5px black";
+        }
+}
+
+function Light_Dark ()
+{   
+document.body.style.background= "url('img/dark_bg.jpg') center no-repeat";
+document.body.style.backgroundSize= "cover";
+var elements = document.getElementsByClassName("sidebar")
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.background="url('img/light_bg.jpg')";
+        }
+
+        var elements = document.getElementsByClassName("sidebar_menu")
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.color="#2f2f2f";
+            elements[i].style.textShadow="none";
+        }
+
+        var elements = document.getElementsByClassName("some_tekst")
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.color="whitesmoke";
+            elements[i].style.textShadow="1px 1px 5px black";
+        }
+}
+
+function Dark_Light ()
+{   
+document.body.style.background= "url('img/light_bg.jpg') center no-repeat";
+document.body.style.backgroundSize= "cover";
+var elements = document.getElementsByClassName("sidebar")
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.background="url('img/dark_bg.jpg')";
+        }
+
+        var elements = document.getElementsByClassName("some_tekst")
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.color="#2f2f2f";
+            elements[i].style.textShadow="none";
+        }
+}
